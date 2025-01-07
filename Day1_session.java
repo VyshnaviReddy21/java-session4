c86b9140874efed56ce5ba74ac6b29d21d5588b3
VyshnaviReddy21 <vyshureddy1133@gmail.com>
2025-01-07 21:30:45 +0530

    first commkit

---
+++ Day1_session.java
@@ -0,0 +1,8 @@
+public class Day1_session {
+    public static void main(String[] args){
+        System.out.println("How\"s you");
+        System.out.println(2*2);
+        System.out.println(5/3);
+
+    }
+}
